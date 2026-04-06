8898caf63ce05a521ff62f34cfc793faf1bfe510
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-04-05 23:33:19 -0400

    11-2 passing

--- city_functions.py
+++ city_functions.py
@@ -1,2 +1,5 @@
-def city_country(city, country, population):
-    return f"{city.title()}, {country.title()} - population {population}"
+def city_country(city, country, population=None):
+    if population:
+        return f"{city.title()}, {country.title()} - population {population}"
+    else:
+        return f"{city.title()}, {country.title()}"
