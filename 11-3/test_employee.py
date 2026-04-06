189c9b1575fdd8d01321e67294b289bc65ef55a6
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-04-05 23:45:12 -0400

    11-3 fixture

--- 11-3/test_employee.py
+++ 11-3/test_employee.py
@@ -1,11 +1,14 @@
+import pytest
 from employee import Employee
 
-def test_give_default_raise():
-    emp = Employee('chloe', 'drakeford', 50000)
-    emp.give_raise()
-    assert emp.salary == 55000
+@pytest.fixture
+def employee():
+    return Employee('chloe', 'drakeford', 50000)
 
-def test_give_custom_raise():
-    emp = Employee('chloe', 'drakeford', 50000)
-    emp.give_raise(10000)
-    assert emp.salary == 60000
+def test_give_default_raise(employee):
+    employee.give_raise()
+    assert employee.salary == 55000
+
+def test_give_custom_raise(employee):
+    employee.give_raise(10000)
+    assert employee.salary == 60000
